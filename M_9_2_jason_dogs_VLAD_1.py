00ea599d7a7937b67889e6589e0567e9eef2bf55
SkyyNikole <elanan7724@gmail.com>
2024-10-17 16:58:32 +0300

    text btn changed

--- M_9_2_jason_dogs_VLAD_1.py
+++ M_9_2_jason_dogs_VLAD_1.py
@@ -11,7 +11,7 @@ window.title('DoggyDogs')
 label = Label(window)
 label.pack()
 
-btn = Button(window, text ='Get_a_Dog_of_your_Dream')
+btn = Button(window, text ='Get_a_Dog_of_Dream')
 btn.pack()
 
 window.mainloop()
