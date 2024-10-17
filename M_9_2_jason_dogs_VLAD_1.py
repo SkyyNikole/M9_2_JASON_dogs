6f4a47e4af2f106d2513be3797a0fb5cdbad1f11
SkyyNikole <elanan7724@gmail.com>
2024-10-17 16:53:36 +0300

    prog_interface: 1) main window 2) label and button

---
+++ M_9_2_jason_dogs_VLAD_1.py
@@ -0,0 +1,19 @@
+import requests
+from tkinter import *
+from tkinter import ttk
+from PIL import Image, ImageTk
+from io import BytesIO
+
+window = Tk()
+window.geometry('500x400')
+window.title('DoggyDogs')
+
+label = Label(window)
+label.pack()
+
+btn = Button(window, text ='Get_a_DoggyDog')
+btn.pack()
+
+window.mainloop()
+
+
