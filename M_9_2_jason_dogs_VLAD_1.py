68ca6614542f8409a211f312659f194923645d84
SkyyNikole <elanan7724@gmail.com>
2024-10-17 17:09:43 +0300

    def get_json_dog() - gets json of a random dog

--- M_9_2_jason_dogs_VLAD_1.py
+++ M_9_2_jason_dogs_VLAD_1.py
@@ -1,8 +1,17 @@
-import requests
+import requests as r
 from tkinter import *
 from tkinter import ttk
 from PIL import Image, ImageTk
 from io import BytesIO
+
+
+def image_dogs_in_tk():
+
+def get_json_dog():
+    api_answer = r.get('https://dog.ceo/api/breeds/image/random')
+    json_dog = api_answer.json()
+    return json_dog['message']
+#_1_получает запрос по api и переводит в формат json
 
 window = Tk()
 window.geometry('500x400')
@@ -11,7 +20,7 @@ window.title('DoggyDogs')
 label = Label(window)
 label.pack()
 
-btn = Button(window, text ='Get_a_Dog_of_Dream')
+btn = Button(window, text ='Get_a_Dog_of_your_Dream', command = image_dogs_in_tk)
 btn.pack()
 
 window.mainloop()
