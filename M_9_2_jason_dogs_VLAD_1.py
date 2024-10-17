6f235f06a4d207e9e6924e6a7a06d1d79ec97513
SkyyNikole <elanan7724@gmail.com>
2024-10-17 17:18:41 +0300

    def image_dogs_in_tk() - btn command

--- M_9_2_jason_dogs_VLAD_1.py
+++ M_9_2_jason_dogs_VLAD_1.py
@@ -6,16 +6,23 @@ from io import BytesIO
 
 
 def image_dogs_in_tk():
+    url_image = get_json_dog()
+#_4_присваиваем адрес переменной url_image
+
 
 def get_json_dog():
     api_answer = r.get('https://dog.ceo/api/breeds/image/random')
     json_dog = api_answer.json()
     return json_dog['message']
-#_1_получает запрос по api и переводит в формат json
+
+#_1_получаем ответ на наш запрос-request по api и ответ запоминаем в переменной
+#_2_json получаем из ответа (message : значение, status : success)
+#_3_возвращаем на функцию адрес - значение по ключу message
 
 window = Tk()
 window.geometry('500x400')
 window.title('DoggyDogs')
+
 
 label = Label(window)
 label.pack()
