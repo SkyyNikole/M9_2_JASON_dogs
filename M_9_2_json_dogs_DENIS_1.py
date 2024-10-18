69ce264d4f1cc998b1041bad3495a3108271e9c6
SkyyNikole <elanan7724@gmail.com>
2024-10-18 08:20:13 +0300

    6) def get_dog_image() 7) modified mb in def show_dog_image()

--- M_9_2_json_dogs_DENIS_1.py
+++ M_9_2_json_dogs_DENIS_1.py
@@ -4,8 +4,26 @@ from PIL import Image, ImageTk
 from io import BytesIO
 from tkinter import messagebox as mb
 
-#_1 функция для получения изображения из интернета и вывода
-# на экран пользователя, сайт dog.ceo присылает не саму картинку,
+
+#_2_функция для получения изображения,
+#_будем по ссылке обращаться к сайту dog.ceo
+def get_dog_image():
+    try:
+        respond_json = r.get('https://dog.ceo/api/breeds/image/random')
+#_2.1_ответ в виде json ({'message' : 'http - адрес изобр', 'status' : 'success'})
+        respond_json.raise_for_status()
+#_2.2_узнали статус, если ок = code 200, если 404 - page not found
+        data = respond_json.json()
+#_2.3_получили ответ в формате json и ниже возращаем https - значение ключа
+        return data('message')
+    except Exception as e:
+        mb.showerror('Error!', 'API request error : {e}.')
+        return None
+#_если ошибка, то возвращаем None - пустоту
+
+
+#_1 функция для загрузки изображения из интернета в метку,
+# сайт dog.ceo присылает не саму картинку,
 # а ссылку на нее, то сначала мы получим ссылку image_url (та ссылка,
 # которая возвращает нам в формате json наш сайт dog.ceo)
 def show_dog_image():
@@ -25,7 +43,7 @@ def show_dog_image():
             label.config(image=img)
             label.image=img
         except Exception as e:
-                mb.showerror('Error!', 'Error type: {e}.')
+                mb.showerror('Error!', 'Image loading error: {e}.')
 
 window = Tk()
 window.title('Lovely_Doggies')
